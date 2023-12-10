f1d7619191d8bf3ba6e1ecdb1ca7c97c12088656
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-10 09:34:28 +0100

    Remove ddcutil for deps

--- elucidate.sh
+++ elucidate.sh
@@ -60,8 +60,8 @@ DISTRO=$(lsb_release -sc)
 DDTL=2.0.0
 
 # Build dependencies, recommended and script-related packages.
-DEPS="acpid arc-theme aspell build-essential ccache check cmake cowsay ddcutil doxygen \
-fonts-noto freeglut3-dev graphviz gstreamer1.0-plugins-bad gstreamer1.0-plugins-good \
+DEPS="acpid arc-theme aspell build-essential ccache check cmake cowsay doxygen fonts-noto\
+freeglut3-dev graphviz gstreamer1.0-plugins-bad gstreamer1.0-plugins-good \
 gstreamer1.0-plugins-ugly imagemagick libaom-dev libasound2-dev libavahi-client-dev \
 libavif-dev libblkid-dev libbluetooth-dev libegl1-mesa-dev libexif-dev \
 libfontconfig-dev libdrm-dev libfreetype-dev libfribidi-dev libgbm-dev libgeoclue-2-dev \
