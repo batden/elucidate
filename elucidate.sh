ad866abb631bf14c0be49ef435c76a6388611d47
Philippe Jean Guillaumie <batden@sfr.fr>
2024-01-08 10:05:26 +0100

    Add gettext to deps

--- elucidate.sh
+++ elucidate.sh
@@ -61,7 +61,7 @@ DDCTL=2.0.0
 
 # Build dependencies, recommended and script-related packages.
 DEPS="acpid arc-theme build-essential ccache check cmake cowsay doxygen fonts-noto \
-freeglut3-dev graphviz gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
+freeglut3-dev gettext graphviz gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly \
 imagemagick libaom-dev libasound2-dev libavahi-client-dev libavif-dev libblkid-dev \
 libbluetooth-dev libegl1-mesa-dev libexif-dev libfontconfig-dev libdrm-dev \
 libfreetype-dev libfribidi-dev libgbm-dev libgeoclue-2-dev libgif-dev \
