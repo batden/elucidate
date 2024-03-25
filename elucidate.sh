96f9d527fc1f85cd2c415688fd7b72bd1ca96b67
Philippe Jean Guillaumie <batden@sfr.fr>
2024-03-25 09:07:52 +0100

    Add places module

--- elucidate.sh
+++ elucidate.sh
@@ -93,6 +93,7 @@ CLONEDI="git clone https://git.enlightenment.org/enlightenment/edi.git"
 CLONENT="git clone https://git.enlightenment.org/vtorri/entice.git"
 CLONEFT="git clone https://git.enlightenment.org/enlightenment/enlightenment-module-forecasts.git"
 CLONEPN="git clone https://git.enlightenment.org/enlightenment/enlightenment-module-penguins.git"
+CLONEPL="git clone https://git.enlightenment.org/enlightenment/enlightenment-module-places.git"
 CLONETE="git clone https://github.com/dimmus/eflete.git"
 
 # “MBS” stands for Meson Build System.
@@ -110,6 +111,7 @@ edi
 entice
 enlightenment-module-forecasts
 enlightenment-module-penguins
+enlightenment-module-places
 eflete"
 
 # Bug reporting: Uncomment the following (remove the leading # character) to force messages to
@@ -201,7 +203,7 @@ cnt_dir() {
   fi
 
   case $COUNT in
-  14)
+  15)
     printf "$BLDG%s $OFF%s\n\n" "All programs have been downloaded successfully."
     beep_dl_complete
     sleep 2
@@ -213,7 +215,7 @@ cnt_dir() {
     exit 1
     ;;
   *)
-    printf "\n$BLDY%s %s\n" "WARNING: ONLY $COUNT OF 14 PROGRAMS HAVE BEEN DOWNLOADED!"
+    printf "\n$BLDY%s %s\n" "WARNING: ONLY $COUNT OF 15 PROGRAMS HAVE BEEN DOWNLOADED!"
     printf "\n$BLDY%s $OFF%s\n\n" "WAIT 12 SECONDS OR HIT CTRL+C TO EXIT NOW."
     beep_attention
     sleep 12
@@ -659,6 +661,8 @@ install_now() {
   $CLONEFT
   echo
   $CLONEPN
+  echo
+  $CLONEPL
   printf "\n\n$BOLD%s $OFF%s\n\n" "Fetching source code from Dimmus' git repository..."
   $CLONETE
   echo
