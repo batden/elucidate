35673b7338123fffc352d2ed4a3a48d72e0ce5d8
Philippe Jean Guillaumie <batden@sfr.fr>
2025-03-26 11:08:39 +0100

    Minor correction

--- elucidate.sh
+++ elucidate.sh
@@ -168,7 +168,7 @@ deps=(
   xdotool
 )
 
-# Source repositories of programs: Latest source code available.
+# Source repositories of programs. Latest source code available:
 clonefl="git clone https://git.enlightenment.org/enlightenment/efl.git"
 clonety="git clone https://git.enlightenment.org/enlightenment/terminology.git"
 clonenl="git clone https://git.enlightenment.org/enlightenment/enlightenment.git"
