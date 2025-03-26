cdf14cb20b96f718c4a01a56bf2211ac9baf52fe
Philippe Jean Guillaumie <batden@sfr.fr>
2025-03-26 13:35:30 +0100

    Minor correction

--- elucidate.sh
+++ elucidate.sh
@@ -12,7 +12,7 @@
 # (DEB packages, if they exist, tend to lag far behind). Once installed,
 # you can update your Enlightenment desktop whenever you like.
 
-# Facultative: Additional steps may be taken to achieve optimal results.
+# Optional: Additional steps may be taken to achieve optimal results.
 # Please refer to the comments of the build_plain() function.
 
 # Tip: Set your terminal scrollback to unlimited so that you can scroll up
